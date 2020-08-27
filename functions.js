33f4dfc4dc844c7a47a9bfb724c3e85fad7dc3d1
ezequiel <ezequiel.alganiaraz@gmail.com>
2020-08-27 08:46:34 -0300

    commit-27-ago-2020

--- functions.js
+++ functions.js
@@ -27,3 +27,24 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
 //   dots[slideIndex-1].className += " active";
 }
+var inicio = 0;
+
+function swipe() {
+  var x = event.touches[0].clientX;
+  
+  console.log(x);
+  inicio = x;
+ 
+}
+
+function end() {
+  var x = event.changedTouches[0].pageX;
+  swp = x * inicio;
+  
+  if (swp > 0){
+    plusSlides(1)
+  }
+  if (swp < 0){
+    plusSlides(-1)
+  }
+}
